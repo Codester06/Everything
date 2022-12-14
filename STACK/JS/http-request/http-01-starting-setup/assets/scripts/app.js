5db8fbd1cc55795da5420293e18b44fc5ffb85fb
The-Codester-Vs <viveksharma55236@gmail.com>
2022-12-14 11:38:02 +0530

    everything

--- STACK/JS/http-request/http-01-starting-setup/assets/scripts/app.js
+++ STACK/JS/http-request/http-01-starting-setup/assets/scripts/app.js
@@ -4,35 +4,62 @@ const form = document.querySelector("#new-post form");
 const fetchButton = document.getElementById("available-posts");
 const postList = document.querySelector('ul');
 
+// function sendHttpRequest(method, url, data) {
+//     const promise = new Promise((resolve, reject) => {
+//         const xhr = new XMLHttpRequest();
+//         xhr.open(method, url);
+//         // console.log(xhr)
+//         xhr.responseType = "json";
+//         xhr.onload = function () {
+//         if (xhr.status >= 200 && xhr.status < 300) {
+//             resolve(xhr.response);
+//         }else {
+//             reject(new Error('Something went wrong!'));
+//         }
+//         };
+
+//         xhr.onerror = function () {
+//             reject(new Error('failed to sent request!'));
+//         };
+
+//         xhr.send(JSON.stringify(data));
+//     });
+//     return promise;
+// }
 
 function sendHttpRequest(method, url, data) {
-    const promise = new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open(method, url);
-        // console.log(xhr)
-        xhr.responseType = "json";
-        xhr.onload = function () {
-            resolve(xhr.response);
-        };
-        xhr.send(JSON.stringify(data));
-    });
-    return promise;
+        return fetch(url,{
+            method: method,
+            body: JSON.stringify(data),
+            headers:{
+                'Content-Type': 'application/json'
+
+            }
+        }).then( response => {
+            return response.json();
+        })
 }
 
 async function fetchPosts() {
-    const responseData = await sendHttpRequest(
-        "GET",
-        "https://jsonplaceholder.typicode.com/posts"
-    );
-
-    const listOfPosts = responseData;
-    for (const post of listOfPosts) {
-        const postEl = document.importNode(postTemplate.content, true);
-        postEl.querySelector("h2").textContent = post.title.toUpperCase();
-        postEl.querySelector("p").textContent = post.body;
-        postEl.querySelector("li").id = post.id;
-        listElement.append(postEl);
+    try {
+        const responseData = await sendHttpRequest(
+            "GET",
+            "https://jsonplaceholder.typicode.com/posts"
+        );
+    
+        const listOfPosts = responseData;
+        for (const post of listOfPosts) {
+            const postEl = document.importNode(postTemplate.content, true);
+            postEl.querySelector("h2").textContent = post.title.toUpperCase();
+            postEl.querySelector("p").textContent = post.body;
+            postEl.querySelector("li").id = post.id;
+            listElement.append(postEl);
+        }
+        
+    } catch (error) {
+        alert(error.message)
     }
+    
 }
 
 async function createPost(title, body) {
