6335d0c9caa8d55c99aac0c8ddd4e3cc2c92bf2b
Vivek sharma <75115122+The-Codester-Vs@users.noreply.github.com>
2023-04-14 15:20:34 +0530

    delete

--- STACK/JavaScript/Lib/lib-01-starting-setup/app.js
+++
@@ -1,5 +0,0 @@
-const customer = ['codester','vivek','codexter']
-
-const activeCustomer = ['codester','vivek']
-const inactiveCustomer = _.difference(customer, activeCustomer)
-console.log(inactiveCustomer) // lodash 
